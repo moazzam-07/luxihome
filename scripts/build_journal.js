a87c84914cfb2ed2151569197d1fe547ddde45af
shayan-human <shayan.command@gmail.com>
2026-09-12 18:34:25 +0530

    feat(branding): update favicon with dual light/dark contrast and adaptive SVG favicon

--- scripts/build_journal.js
+++ scripts/build_journal.js
@@ -950,6 +950,8 @@ function generateArticleHtml(art) {
     <meta property="og:image" content="${art.image}">
     <meta name="twitter:card" content="summary_large_image">
 
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
@@ -1107,6 +1109,8 @@ function generateHubHtml() {
     <meta property="og:site_name" content="LUXiHOME">
     <meta name="twitter:card" content="summary_large_image">
 
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
